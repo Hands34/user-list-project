86e5007c9b5f5849c0bce74cb6613e76ac0eab2a
Hands34 <m.cook2434@gmail.com>
2023-02-08 18:02:15 -0600

    added state management to remove the error modal when clicked

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -7,13 +7,23 @@ import ErrorModal from "../UI/ErrorModal";
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
+  const [isError, setIsError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setIsError({
+        title: "Invalid input",
+        message: "Please enter a valid name and age. (Must not be blank)",
+      });
       return;
     }
     if (+enteredAge < 1) {
+      setIsError(true);
+      setIsError({
+        title: "Invalid age",
+        message: "Please enter a valid age. (Must be higher than 0.",
+      });
       return;
     }
     props.onAddUser(enteredUsername, enteredAge);
@@ -29,9 +39,18 @@ const AddUser = (props) => {
     setEnteredAge(event.target.value);
   };
 
+  const errorHandler = () => {
+    setIsError(null);
+  };
   return (
     <div>
-      <ErrorModal title="An error occured!" message="Something went wrong!" />
+      {isError && (
+        <ErrorModal
+          title={isError.title}
+          message={isError.message}
+          onConfirm={errorHandler}
+        />
+      )}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
